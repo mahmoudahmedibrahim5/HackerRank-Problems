9b5a453a356a56b070983e88201de2f1fc7bd948
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-01-02 15:26:05 +0200

    Passed All Testcases

--- C++/Meduim/Attribute Parser.cpp
+++ C++/Meduim/Attribute Parser.cpp
@@ -4,26 +4,52 @@
 
 using namespace std;
 
-typedef struct 
+typedef struct tag
 {
     string name;
-    void* child;
+    vector<tag*> children;
+
     int attributeCount;
     string* attributeName;
     string* attributeValue;
+    
     bool status;
     bool childStatus;
 }Tag;
 
+/* Status */
 #define OPENED  false
 #define CLOSED  true
+/* Child Status */
+#define PARENT  false
+#define CHILD   true
+
+vector<Tag> readTags(int N);
+string evaluateQuery(vector<Tag> tags, string line);
 
 int main()
 {
     int N, Q;
     cin >> N >> Q;
+
+    vector<Tag> tags = readTags(N);
+
+    while(Q--)
+    {
+        string line;
+        cin >> line;
+        cout << evaluateQuery(tags, line) << endl;
+    }
+    
+    return 0;
+}
+
+vector<Tag> readTags(int N)
+{
     vector<Tag> tags(N/2);
     int tagIndex = 0;
+    int openTags = 0;
+
     while (N--)
     {
         string line[50];
@@ -32,134 +58,140 @@ int main()
             cin >> line[i];
             int n = line[i].size();
             if(line[i][n-1] == '>'){
-                attributesCount = i;
+                attributesCount = i/3;
+                line[i].erase(line[i].end() - 1);
                 break;
             }
         }
 
         if(line[0][1] == '/')
         {
+            // Remove </ 
             line[0].erase(line[0].begin());
             line[0].erase(line[0].begin());
-            line[0].erase(line[0].end()-1);
 
+            // Search for the tag to close it
             for(int i = 0; i < tagIndex; i++){
                 if(tags[i].name == line[0]){
                     tags[i].status = CLOSED;
                     break;
                 }
             }
+            openTags--;
         }
         else
         {
             /* Initialize the tag */
-            line[0].erase(line[0].begin());
-            if(attributesCount == 1)
-                line[0].erase(line[0].end() - 1);
+            line[0].erase(line[0].begin()); // remove <
             tags[tagIndex].name = line[0];
-            tags[tagIndex].attributeCount = attributesCount/3;
+            
+            tags[tagIndex].attributeCount = attributesCount;
             tags[tagIndex].attributeName = new string[tags[tagIndex].attributeCount];
             tags[tagIndex].attributeValue = new string[tags[tagIndex].attributeCount];
+            
             tags[tagIndex].status = OPENED;
-            tags[tagIndex-1].childStatus = false;
+            if(openTags == 0)
+                tags[tagIndex].childStatus = PARENT;
+            else
+                tags[tagIndex].childStatus = CHILD;
 
             /* Assign attributes of the tag */
-            for(int i = 0; i < attributesCount/3; i++){
+            for(int i = 0; i < attributesCount; i++){
                 tags[tagIndex].attributeName[i] = line[(i*3) + 1];
-                line[(i*3) + 3].erase(line[(i*3) + 3].end() - 1);
+                // remove ""
+                line[(i*3) + 3].erase(line[(i*3) + 3].end() - 1); 
                 line[(i*3) + 3].erase(line[(i*3) + 3].begin());
+                
                 tags[tagIndex].attributeValue[i] = line[(i*3) + 3];
             }
-            tags[tagIndex].attributeValue[tags[tagIndex].attributeCount - 1].erase(tags[tagIndex].attributeValue[tags[tagIndex].attributeCount - 1].end()-1);
 
             /* Child */
-            if(tagIndex > 0){
-                if(tags[tagIndex-1].status == OPENED){
-                    tags[tagIndex-1].child = (void *)&tags[tagIndex];
-                    tags[tagIndex-1].childStatus = true;
+            for(int i = tagIndex - 1; i >= 0; i--)
+            {
+                if(tags[i].status == OPENED){
+                    tags[i].children.push_back(&tags[tagIndex]);
+                    break;
                 }
             }
-
             tagIndex++;
+            openTags++;
         }
         
     }
+    return tags;
+}
 
+string evaluateQuery(vector<Tag> tags, string line)
+{
     int index = 0;
-    while(Q--)
+    string result = "Not Found!";
+
+    string tagName;
+    while(line[index] != '.' && line[index] != '~')
+        tagName.push_back(line[index++]);
+    
+    Tag* currentTag;
+    for(int i = 0; i < (int)tags.size(); i++){
+        if(tags[i].name == tagName){
+            currentTag = &tags[i];
+            break;
+        }
+    }
+
+    if(currentTag->childStatus == PARENT)
     {
-        index = 0;
-
-        string line;
-        cin >> line;
-
-        string tagName;
-        while(line[index] != '.' && line[index] != '~')
-            tagName.push_back(line[index++]);
-        
-        Tag* currentTag;
-        for(int i = 0; i < (int)tags.size(); i++){
-            if(tags[i].name == tagName){
-                currentTag = &tags[i];
-                break;
-            }
-        }
-
         bool child;
-        if(line[index] == '.'){
+        if(line[index] == '.')
             child = true;
-            currentTag = (Tag *)currentTag->child;
-        }
         else if(line[index] == '~')
             child = false;
-        else
-            cout << "Error" << endl << "Error" << endl << "Error" << endl;
 
         bool found = true;
+        index++;
+        
         if(child)
         {
             tagName.clear();
-            while (child)
+            while (1)
             {
-                index++;
                 if(line[index] == '~'){
+                    found = false;
+                    for(int i = 0; i < (int)currentTag->children.size(); i++){
+                        if(currentTag->children[i]->name == tagName){
+                            found = true;
+                            currentTag = currentTag->children[i];
+                            break;
+                        }
+                    }
                     break;
                 }
                 else if(line[index] == '.'){
-                    //currentTag = (Tag *)currentTag->child;
+                    found = false;
+                    for(int i = 0; i < (int)currentTag->children.size(); i++){
+                        if(currentTag->children[i]->name == tagName){
+                            found = true;
+                            currentTag = currentTag->children[i];
+                            break;
+                        }
+                    }
+                    if(!found)
+                        break;
                     tagName.clear();
                 }
                 else{
                     tagName.push_back(line[index]);
                 }
+                index++;
             }
             index++;
-            
-            found = false;
-            for(int i = 0; i < (int)tags.size(); i++){
-                if(tags[i].name == tagName){
-                    currentTag = &tags[i];
-                    found = true;
-                    break;
-                }
-            }
-        }
-        else
-            index++;
-            
-        string result = "Not Found!";
-        if(!found){
-
-        }
-        else{
+        }    
+        
+        if(found)
+        {
             string attName;
-            for(int i = index; i < (int)line.size(); i++){
+            for(int i = index; i < (int)line.size(); i++)
                 attName.push_back(line[i]);
-            }
-            // cout << attName << endl;
-            // cout << currentTag->name << endl;
 
-            
             for(int i = 0; i < currentTag->attributeCount; i++){
                 if(currentTag->attributeName[i] == attName){
                     result = currentTag->attributeValue[i];
@@ -167,21 +199,7 @@ int main()
                 }
             }
         }
-        cout << result << endl;
     }
 
-    // cout << endl;
-    // cout << "/************** Tags View **************/" << endl;
-    // for(int i = 0; i < (int)tags.size(); i++){
-    //     cout << tags[i].name << endl;
-    //     cout << "attributes count = " << tags[i].attributeCount << endl;
-    //     for(int j = 0; j < tags[i].attributeCount; j++){
-    //         cout << "attribute name = " << tags[i].attributeName[j] << ", and its value = " << tags[i].attributeValue[j] << endl;
-    //     }
-    //     cout << "Status = " << tags[i].status << endl << endl;
-    //     cout << "Childs status = " << tags[i].childStatus << endl << endl;
-    // }
-    
-
-    return 0;
+    return result;
 }
