9937fdf50df53e3def47ba619785b7e7bee4ce6a
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-11-11 17:29:58 +0200

    Passed All Testcases

--- C++/Meduim/Attending Workshops.cpp
+++ C++/Meduim/Attending Workshops.cpp
@@ -3,95 +3,54 @@
 using namespace std;
 
 //Define the structs Workshops and Available_Workshops.
-typedef struct Workshop{
+struct Workshop{
     int startTime;
     int duration;
-    int endTime;
-}Workshop;
+    
+    bool operator<(const Workshop& other) const{
+        if((startTime + duration) == (other.startTime + other.duration))
+            return startTime > other.startTime;
+        else
+            return (startTime + duration) < (other.startTime + other.duration);
+    }
+};
 
-typedef struct{
+struct Available_Workshops{
     int n;
-    Workshop* arr;
-    int smallest;
-}Available_Workshops;
+    Workshop* arr;    
+};
 
 //Implement the functions initialize and CalculateMaxWorkshops
-Available_Workshops* initialize(int start_time[], int duration[], int n)
+Available_Workshops* initialize(int startTime[], int duration[], int n)
 {
     Available_Workshops* result = new Available_Workshops;
-    result->n = n;
     result->arr = new Workshop[n];
-    result->smallest = start_time[0] + duration[0];
     
+    vector<Workshop> arr(n);
+    for(int i = 0; i < n; i++){
+        arr[i].startTime = startTime[i];
+        arr[i].duration = duration[i];
+    }
+    sort(arr.begin(), arr.end());
+    
+    int endTime;
+    endTime = arr[0].startTime + arr[0].duration;
+    result->n = 1;
     for(int i = 0; i < n; i++)
     {
-        result->arr[i].startTime = start_time[i];
-        result->arr[i].duration = duration[i];
-        result->arr[i].endTime = start_time[i] + duration[i];
-        if(result->arr[i].endTime < result->smallest)
-            result->smallest = result->arr[i].endTime;
+        if(arr[i].startTime >= endTime){
+            endTime = arr[i].startTime + arr[i].duration;
+            result->arr[result->n - 1] = arr[i];
+            result->n++;
+        }
     }
-    return result;    
-}
-// vector<vector<int>> memoize;
-// int recursive(int endTime, Available_Workshops* ptr)
-// {
-//     for(int i = 0; i < (int)memoize.size(); i++){
-//         if(memoize.at(i).at(0) == endTime)
-//             return memoize.at(i).at(1) + 1;
-//     }
     
-//     int result = 0;
-//     int test;
-//     for(int i = 0; i < ptr->n; i++){
-//         if(ptr->arr[i].startTime >= endTime){
-//             test = recursive(ptr->arr[i].endTime, ptr);
-//             if(test > result)
-//                 result = test;
-//         }
-//     }
-//     vector<int> element;
-//     element.push_back(endTime);
-//     element.push_back(result);
-//     return result + 1;
-// }
+    return result;
+}
+
 int CalculateMaxWorkshops(Available_Workshops* ptr)
 {
-    Workshop tmp;
-    for(int i = 0; i < ptr->n; i++)
-    {
-        for(int j = 0; j < ptr->n - 1 - i; j++)
-        {
-            if(ptr->arr[j].endTime > ptr->arr[j+1].endTime)    
-            {
-                tmp = ptr->arr[j];
-                ptr->arr[j] = ptr->arr[j+1];
-                ptr->arr[j+1] = tmp;
-            }
-        }
-    }
-    
-    int result;
-    int max = 0;
-    int end;
-    
-    for(int i = 0; i < ptr->n; i++)
-    {
-        if(ptr->smallest <= ptr->arr[i].startTime)
-            continue;
-        result = 1;
-        end = ptr->arr[i].endTime;
-        for (int j = i + 1; j < ptr->n; j++)
-        {
-            if(ptr->arr[j].startTime >= end){
-                result++; 
-                end = ptr->arr[j].endTime;
-            }
-        }
-        if(result > max)
-            max = result;
-    }
-    return max;
+    return ptr->n;
 }
 
 int main(int argc, char *argv[]) {
